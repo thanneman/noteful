55e33443221b7c334fdaaff9977d466d7fd1ec37
Travis Hanneman <thanneman1@gmail.com>
2019-12-17 20:19:20 -0700

    Refactored App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import Header from './Header'
-import Nav from './Nav'
-import NoteArea from './NoteArea'
-import Note from './Note'
-import NotesContext from './NotesContext'
-import './App.css'
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import Nav from './Nav';
+import NoteArea from './NoteArea';
+import Note from './Note';
+import AddFolder from './AddFolder';
+import AddNote from './AddNote';
+import NotesContext from './NotesContext';
+import FolderError from './FolderError';
+import PropTypes from 'prop-types';
+import './App.css';
 
 class App extends Component {
-  state = {
-    folders: [],
-    notes: []
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      folders: [
+      ],
+      notes: [
+      ]
+    };
+  }
+
+  deleteNote = noteId => {
+    const newNotes = this.state.notes.filter (note => note.id !== noteId)
+    let deleteUrl = `http://localhost:9090/notes/${noteId}`;
+
+    fetch(deleteUrl, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'applicaion/json'
+      },
+    })
+    .then(res => {
+      if(!res.ok) {
+        throw new Error(res.status)
+      }
+      return res.json()
+    })
+    .catch(error => this.setState({ error }))
+
+    this.setState({
+      notes: newNotes
+    })
+  }
 
   componentDidMount() {
+    let foldersUrl = 'http://localhost:9090/folders';
+    let notesUrl = 'http://localhost:9090/notes';
+
     Promise.all([
-      fetch('http://localhost:9090/folders'),
-      fetch('http://localhost:9090/notes')
+      fetch(foldersUrl),
+      fetch(notesUrl)
     ])
     .then(([foldersRes, notesRes]) => {
       if (!foldersRes.ok)
@@ -34,25 +69,44 @@ class App extends Component {
     })
   }
 
-  deleteNote = noteId => {
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== noteId)
-    });
-  };
+  updateLists = () => {
+    let foldersUrl = 'http://localhost:9090/folders';
+    let notesUrl = 'http://localhost:9090/notes';
+
+    Promise.all([
+      fetch(foldersUrl),
+      fetch(notesUrl)
+    ])
+    .then(([foldersRes, notesRes]) => {
+      if (!foldersRes.ok)
+        return foldersRes.json().then(e => Promise.reject(e));
+      if (!notesRes.ok) 
+        return notesRes.json().then(e => Promise.reject(e));
+
+      return Promise.all([foldersRes.json(), notesRes.json()]);
+    })
+    .then(([folders, notes]) => {
+      this.setState({folders, notes});
+    })
+    .catch(error => {
+      console.log({error});
+    })
+  }
 
   render() {
     const contextValue = {
       folders: this.state.folders,
       notes: this.state.notes,
       deleteNote: this.deleteNote,
+      updateLists: this.updateLists,
     }
 
     return (
       <NotesContext.Provider value={contextValue}>
         <div className='App'>
           <Header />
-          <Switch>
             <main>
+              <FolderError>
               <Route 
                 exact path='/'
                 component={Nav}
@@ -78,12 +132,32 @@ class App extends Component {
                 path='/note/:noteId'
                 component={Note}
               />
+              <Route
+                path='/add-folder'
+                component={Nav}
+              />
+              <Route
+                path='/add-folder'
+                component={AddFolder}
+              />
+              <Route
+                path='/add-note'
+                component={Nav}
+              />
+              <Route
+                path='/add-note'
+                component={AddNote}
+              />
+              </FolderError>
             </main>
-          </Switch>
         </div>
       </NotesContext.Provider>
     );
   }
+}
+
+NotesContext.Provider.propTypes = {
+  value: PropTypes.object,
 }
 
 export default App;
